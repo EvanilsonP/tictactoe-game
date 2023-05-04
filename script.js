442604583368d2bedfada066edbc9fee75976ec8
EvanilsonP <pevanilson99@gmail.com>
2023-05-04 16:03:56 -0300

    wrap-up

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ use it later on
 */
 const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
-let currentPlayer = 'X';
+let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 /*
 We set the inital message to let the players know whose turn it is
@@ -18,140 +19,143 @@ function handleCellPlayed() {
 function handlePlayerChange() {
 
 }
+function handleResultValidation() {
+
+}
 function handleCellClick() {
 
 }
 function handleRestartGame() {
 
 }
-
-document.querySelectorAll('.cell').forEach(cell => 
-cell.addEventListener('click', handleCellClick));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 
 function handleCellClick(clickedCellEvent) {
-/*
-We will save the clicked html element in a variable for easier further use
-*/ 
-const clickedCell = clickedCellEvent.target;
-/*
-Here we will grab the 'data-cell-index' attribute from the clicked cell to identify where that cell is in our grid. 
-Please note that the getAttribute will return a string value. Since we need an actual number we will parse it to an 
-integer(number)
-*/
-const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-/* 
-Next up we need to check whether the call has already been played, 
-or if the game is paused. If either of those is true we will simply ignore the click.
-*/
-if(gameState[clickedCell] !== "" || !gameActive) {
-    return;
-}
-/* 
-If everything is in order we will proceed with the game flow
-*/ 
-handleCellPlayed(clickedCell, clickedCellIndex);
-handleResultValidation();
-};
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-/*
-We update our internal game state to reflect the played move, 
-as well as update the user interface to reflect the played move
-*/
-gameState[clickedCellIndex] = currentPlayer;
-clickedCell.innerHTML = currentPlayer;
-}
-
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleResultValidation() {
-    let roundWon = false;
-    for(let i = 0; i <= 7; i++) {
-        const winCondtion = winningConditions[i];
-        let a = gameState[winCondtion[0]];
-        let b = gameState[winCondtion[1]];
-        let c = gameState[winCondtion[2]];
-        
-        if(a === '' || b === '' || c === '') {
-            continue;
+    /*
+    We will save the clicked html element in a variable for easier further use
+    */    
+        const clickedCell = clickedCellEvent.target;
+    /*
+    Here we will grab the 'data-cell-index' attribute from the clicked cell to identify where that cell is in our grid. 
+    Please note that the getAttribute will return a string value. Since we need an actual number we will parse it to an 
+    integer(number)
+    */
+        const clickedCellIndex = parseInt(
+          clickedCell.getAttribute('data-cell-index')
+        );
+    /* 
+    Next up we need to check whether the call has already been played, 
+    or if the game is paused. If either of those is true we will simply ignore the click.
+    */
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
         }
-
-        if(a === b && b === c) {
-            roundWon = true;
-            break;
-        }
-    };
-
-    if(roundWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
+    /* 
+    If everything if in order we will proceed with the game flow
+    */    
+        handleCellPlayed(clickedCell, clickedCellIndex);
+        handleResultValidation();
     }
-}
 
-const winningConditions2 = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleResultValidation() {
-    let roundWon = false;
-    for(let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i];
-        let a = gameState[winCondition[0]];
-        let b = gameState[winCondition[1]];
-        let c = gameState[winCondition[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
+    function handleCellPlayed(clickedCell, clickedCellIndex) {
+        /*
+        We update our internal game state to reflect the played move, 
+        as well as update the user interface to reflect the played move
+        */
+            gameState[clickedCellIndex] = currentPlayer;
+            clickedCell.innerHTML = currentPlayer;
         }
-        if (a === b && b === c) {
-            roundWon = true;
-            break;
+
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        function handleResultValidation() {
+            let roundWon = false;
+            for (let i = 0; i <= 7; i++) {
+                const winCondition = winningConditions[i];
+                let a = gameState[winCondition[0]];
+                let b = gameState[winCondition[1]];
+                let c = gameState[winCondition[2]];
+                if (a === '' || b === '' || c === '') {
+                    continue;
+                }
+                if (a === b && b === c) {
+                    roundWon = true;
+                    break
+                }
+            }
+        if (roundWon) {
+                statusDisplay.innerHTML = winningMessage();
+                gameActive = false;
+                return;
+            }
         }
-    }
-    if(roundWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
-    }
-/* 
-We will check weather there are any values in our game state array 
-that are still not populated with a player sign
-*/
-let roundDraw = !gameState.includes("");
-gameActive = false;
-return;
-/*If we get to here we know that the no one won the game yet, 
-and that there are still moves to be played, so we continue by changing the current player.
-*/
-handlePlayerChange();
-}
 
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
-}
+        const winningConditions2 = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        function handleResultValidation() {
+            let roundWon = false;
+            for (let i = 0; i <= 7; i++) {
+                const winCondition = winningConditions[i];
+                let a = gameState[winCondition[0]];
+                let b = gameState[winCondition[1]];
+                let c = gameState[winCondition[2]];
+                if (a === '' || b === '' || c === '') {
+                    continue;
+                }
+                if (a === b && b === c) {
+                    roundWon = true;
+                    break
+                }
+            }
+        if (roundWon) {
+                statusDisplay.innerHTML = winningMessage();
+                gameActive = false;
+                return;
+            }
+        /* 
+        We will check weather there are any values in our game state array 
+        that are still not populated with a player sign
+        */
+            let roundDraw = !gameState.includes("");
+            if (roundDraw) {
+                statusDisplay.innerHTML = drawMessage();
+                gameActive = false;
+                return;
+            }
+        /*
+        If we get to here we know that the no one won the game yet, 
+        and that there are still moves to be played, so we continue by changing the current player.
+        */
+            handlePlayerChange();
+        }
 
-function handleRestartGame() {
-    gameActive = true;
-    currentPlayer = 'X';
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
-};
+        function handlePlayerChange() {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            statusDisplay.innerHTML = currentPlayerTurn();
+        }
+
+        function handleRestartGame() {
+            gameActive = true;
+            currentPlayer = "X";
+            gameState = ["", "", "", "", "", "", "", "", ""];
+            statusDisplay.innerHTML = currentPlayerTurn();
+            document.querySelectorAll('.cell')
+                       .forEach(cell => cell.innerHTML = "");
+        }
