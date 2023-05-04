be2f1aa8da523403414f434b30fdb7ef1ed10895
EvanilsonP <pevanilson99@gmail.com>
2023-05-04 15:49:59 -0300

    handling handlePlayerChange function

--- script.js
+++ script.js
@@ -98,3 +98,52 @@ function handleResultValidation() {
         return;
     }
 }
+
+const winningConditions2 = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleResultValidation() {
+    let roundWon = false;
+    for(let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+/* 
+We will check weather there are any values in our game state array 
+that are still not populated with a player sign
+*/
+let roundDraw = !gameState.includes("");
+gameActive = false;
+return;
+/*If we get to here we know that the no one won the game yet, 
+and that there are still moves to be played, so we continue by changing the current player.
+*/
+handlePlayerChange();
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
